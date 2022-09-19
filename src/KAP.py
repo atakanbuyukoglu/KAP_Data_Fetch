a2c438d03177ea8399bd86cdbb3c7800889ec6d2
Atakan Buyukoglu <atakan.buyukoglu@fau.de>
2022-09-19 23:57:37 +0200

    Fetches data only when needed

--- src/KAP.py
+++ src/KAP.py
@@ -2,15 +2,28 @@ import requests as r
 import json
 from pathlib import Path
 from bs4 import BeautifulSoup
+import os
 
 class KAP():
 
     def __init__(self) -> None:
         pass
     
-    def get_company_list(self):
-        # Get the site
+    def get_company_list(self, online=False):
+        
+        resp_html_path = Path(__file__).parents[1] / 'Data' / 'Company_List.html'
         constants_path = Path(__file__).parents[1] / 'Data' / 'Constants.json'
+
+        # Check if the data is available
+        if not online:
+            data_available = resp_html_path.is_file()
+            data_available = data_available and os.path.getsize(resp_html_path) > 0
+            if data_available:
+                with open(resp_html_path, 'r') as f:
+                    html_data = f.read()
+                    return html_data
+
+        # Get the site
         with open(constants_path, 'r') as f:
             constants = json.load(f)
         company_list_site = constants['sites']['company_list']
@@ -19,8 +32,15 @@ class KAP():
         raw_response = r.get(company_list_site)
         # Check the status code
         raw_response.raise_for_status()
+
+        # Filter the results
         response = BeautifulSoup(raw_response.text, 'html.parser')
         resp_filter = response.find_all('div', 'w-clearfix w-inline-block comp-row')
 
-        return resp_filter[0]
+        # Save the HTML results to a log file
+        with open(resp_html_path, 'w+', encoding='utf-8') as f:
+            for company in resp_filter:
+                f.write(str(company) + '\n\n')
+        with open(resp_html_path, 'r', encoding='utf-8') as f:
+            return f.read()
     
